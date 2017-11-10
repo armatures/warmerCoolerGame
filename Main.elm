648815325e07734a41ec3b59e6c80f039779e798
Charlie Bevis <charliebevis@gmail.com>
2017-11-09 22:13:52 -0600

    fun svg exploration

--- Main.elm
+++ Main.elm
@@ -1,20 +1,33 @@
 module Main exposing (..)
 
 import Html exposing (Html, div, text, program)
+import Html.Attributes exposing (style)
 import Mouse
 import Keyboard
+import Plot
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 
 -- MODEL
 
 
 type alias Model =
-    Int
+    { counter: Int
+    , clicks : List Mouse.Position
+    , height : Int
+    , width : Int
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( 0, Cmd.none )
+    ( {counter = 0
+      , clicks = []
+      , height = 600
+      , width = 600
+      }
+    , Cmd.none )
 
 
 
@@ -32,9 +45,20 @@ type Msg
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ text (toString model) ]
-
+   div []
+         [ svg
+          [ version "1.1", baseProfile "full", width (toString model.width), height (toString model.height)]
+          ([ rect [width "100%", height "100%", fill "green"] []
+          ] ++ (showClicks model.clicks))
+         , div [] <| List.map showClick model.clicks
+         ]
+showClicks =
+    let show point =
+          circle [cx (toString point.x), cy (toString point.y), r "40", fill "white"][]
+    in List.map show
+showClick : a -> Html msg
+showClick =
+    (Html.text << toString)
 
 
 -- UPDATE
@@ -42,12 +66,20 @@ view model =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        MouseMsg position ->
-            ( model + 1, Cmd.none )
+    let updateClicks pos =
+            if pos.x < model.width && pos.y < model.height then
+                pos :: model.clicks
+            else
+                []
+    in
+        case msg of
+            MouseMsg position ->
+                ( { model | counter = model.counter + 1
+                  , clicks = updateClicks position
+                  }, Cmd.none )
 
-        KeyMsg code ->
-            ( model + 2, Cmd.none )
+            KeyMsg code ->
+                ( {model | counter = model.counter + 2}, Cmd.none )
 
 
 
@@ -60,7 +92,6 @@ subscriptions model =
         [ Mouse.clicks MouseMsg
         , Keyboard.downs KeyMsg
         ]
-
 
 
 -- MAIN
